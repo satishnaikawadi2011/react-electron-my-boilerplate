d389099944b7aa346526888ddbdcad88a163723b
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-01-18 18:45:48 +0530

    Got working build by using HashRouter instead of BrowseRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
